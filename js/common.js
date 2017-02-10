b18788da0164fdb4a335b0a40433949d23c03179
noxonsu <myzonemailru@gmail.com>
2017-02-11 00:53:37 +0200

    auth popup

--- js/common.js
+++ js/common.js
@@ -21,4 +21,22 @@ $(document).ready(function() {
 		$(this).next().slideToggle();
 	});
 
+	// Popup window
+
+	$('.popup-with-move-anim').magnificPopup({
+		type: 'inline',
+
+		fixedContentPos: false,
+		fixedBgPos: true,
+
+		overflowY: 'auto',
+
+		closeBtnInside: true,
+		preloader: false,
+		
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-slide-bottom'
+	});
+
 });
