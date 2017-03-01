bc558533fe02a7ad1359a324c4e5cf7e695f2616
J0xFlash <joxsea@mail.ru>
2017-03-01 16:18:21 +0200

    v 1.1.6
    Добавлена проверка на баланс банка.

--- games/Hack-DAO-game/game_hack_dao/javascript/main.js
+++ games/Hack-DAO-game/game_hack_dao/javascript/main.js
@@ -1,6 +1,6 @@
 var _W = 1280;
 var _H = 720;
-var version = "v. 1.1.5"
+var version = "v. 1.1.6"
 var login_obj = {};
 var dataAnima = [];
 var dataMovie = [];
@@ -31,6 +31,7 @@ var options_txt_offset = 0;
 var ERROR_KEYTHEREUM = 1;
 var ERROR_TRANSACTION = 2;
 var ERROR_KEY = 3;
+var ERROR_BANK = 4;
 
 var raf = window.requestAnimationFrame || window.webkitRequestAnimationFrame
     || window.mozRequestAnimationFrame || window.oRequestAnimationFrame
