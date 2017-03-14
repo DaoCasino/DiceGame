e83d63b62b0718c1d351370e94683fcd3ad30340
J0xFlash <joxsea@mail.ru>
2017-03-14 12:21:37 +0200

    test

--- games/Hack-DAO-game/game_hack_dao/javascript/ScrGame.js
+++ games/Hack-DAO-game/game_hack_dao/javascript/ScrGame.js
@@ -1640,7 +1640,7 @@ ScrGame.prototype.update = function() {
 		this.timeTotal += diffTime;
 		this.tfTotalTime.setText(Math.round(this.timeTotal/1000));
 	}
-	console.log("this.bSendReques:", this.bSendReques);
+	console.log("this.bSendRequest:", this.bSendRequest);
 	if(obj_game["balance"]==0){
 		this.timeGetResult += diffTime;
 		if(this.timeGetResult >= TIME_GET_RESULT &&
