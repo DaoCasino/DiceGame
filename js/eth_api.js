5ce41eb01fcc2ba0df08035d5168241bf5c60490
noxonsu <myzonemailru@gmail.com>
2017-03-16 11:09:57 +0200

    Add Coder
    # Conflicts:
    #	games/blackjack/game/javascript/main.js

--- js/eth_api.js
+++ js/eth_api.js
@@ -1,4 +1,4 @@
-﻿var buf,EthereumTx,keyethereum,ethUtil,web3;
+﻿var buf,EthereumTx,keyethereum,ethUtil,web3,Coder;
 	
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (Buffer){
@@ -45895,7 +45895,7 @@ SolidityCoder.prototype.getSolidityTypes = function (types) {
     });
 };
 
-var coder = new SolidityCoder([
+coder = new SolidityCoder([
     new SolidityTypeAddress(),
     new SolidityTypeBool(),
     new SolidityTypeInt(),
