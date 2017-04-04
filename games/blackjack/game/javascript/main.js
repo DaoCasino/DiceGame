8f1cfb2260467b910a28a7ffbc2798b7804c3f9e
J0xFlash <joxsea@mail.ru>
2017-04-04 18:32:14 +0200

    v. 1.0.27
    new contract

--- games/blackjack/game/javascript/main.js
+++ games/blackjack/game/javascript/main.js
@@ -1,6 +1,6 @@
 var _W = 1920;
 var _H = 1080;
-var version = "v. 1.0.26"
+var version = "v. 1.0.27"
 var login_obj = {};
 var dataAnima = [];
 var dataMovie = [];
@@ -18,7 +18,8 @@ var rndBg = String(Math.ceil(Math.random()*2));
 
 var addressContract = "0xa65d59708838581520511d98fb8b5d1f76a96cad";
 var	addressRpcContract = "0xc0951cf7a33d26642c8c48d308345a5444562e15";
-var	addressTestContract = "0xfa4f75db04222b0057f1b0b4fa23e55e55e1fde9";
+// var	addressTestContract = "0xfa4f75db04222b0057f1b0b4fa23e55e55e1fde9"; // work
+var	addressTestContract = "0x9b27c1e0ac356b8eef9eebe514f3599f367643b2";
 
 var options_debug = false;
 var options_test = false;
