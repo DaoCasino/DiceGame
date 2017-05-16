0b1cf9d620ff54c5bd2eb06f79a11f5a28fcbff4
J0xFlash <joxsea@mail.ru>
2017-05-16 16:18:46 +0200

    refresh

--- js/configuration.js
+++ js/configuration.js
@@ -173,27 +173,33 @@ var erc20abi = [{
 }]
 
 erc20address = "none"; 
- addressContract = "none";
- urlInfura = "none";
+urlInfura = "none";
+addressDice = "none";
+addressBJDeck = "none";
+addressBJStorage = "none";
+addressBJ = "none";
 
 function changeNet(netname){
+	console.log("net:", netname);
 	if(netname == "ropsten"){
-		 erc20address = "0x95a48dca999c89e4e284930d9b9af973a7481287";
- addressContract = "0x6a8f29e3d9e25bc683a852765f24ecb4be5903fc"
- urlInfura = "https://ropsten.infura.io/JCnK5ifEPH9qcQkX0Ahl";
-
-
+		urlInfura = "https://ropsten.infura.io/JCnK5ifEPH9qcQkX0Ahl";
+		erc20address = "0x95a48dca999c89e4e284930d9b9af973a7481287";
+		// dice
+		addressDice = "0x6a8f29e3d9e25bc683a852765f24ecb4be5903fc";
+		// blackjack
+		addressBJDeck = "0x75dacdec23342b26ff598e3304d3ff632b42077a";
+		addressBJStorage = "0x18d4bd271a6123335edca33eec83318b75ae8ae0";
+		addressBJ = "0x1903eef30317204fb5aabd9533659d9b23a7ec37";
 	} else if(netname == "testrpc"){
-		erc20address = "0xb207301c77a9e6660c9c2e5e8608eaa699a9940f"; 
- addressContract = "0xdbdb232171a8639603e1341b8d62a5425f5d2ddd";
- urlInfura = "http://46.101.244.101:8545";
-
+		urlInfura = "http://46.101.244.101:8545";
+		erc20address = "0xb207301c77a9e6660c9c2e5e8608eaa699a9940f";
+		// dice
+		addressDice = "0xdbdb232171a8639603e1341b8d62a5425f5d2ddd";
 	} else if (netname == "mainnet"){
-		 erc20address = ""; 
- addressContract = "";
- urlInfura = "https://mainnet.infura.io/JCnK5ifEPH9qcQkX0Ahl";
-
+		urlInfura = "https://mainnet.infura.io/JCnK5ifEPH9qcQkX0Ahl";
+		erc20address = ""; 
+		// dice
+		addressDice = "";
 	}
-	console.log("net:", netname)
 }
 changeNet("ropsten");
--- js/erc20call.js
+++ js/erc20call.js
@@ -170,13 +170,14 @@ var erc20abi = [{
     "type": "event"
 }]
 
-var erc20address = "0x95a48dca999c89e4e284930d9b9af973a7481287"; // testnet
-// var erc20address = "0xb207301c77a9e6660c9c2e5e8608eaa699a9940f"; //testrpc
-
-
 var openkey = localStorage.getItem('openkey');
-function approve(approveValue) {
-    console.log("approve")
+
+function getAllowance(addressContract) {
+	if(openkey == undefined){
+		return false;
+	}
+	
+    var _allowance;
     $.ajax({
         type: "POST",
         url: urlInfura,
@@ -193,60 +194,72 @@ function approve(approveValue) {
             }, "latest"]
         }),
         success: function (d) {
-            var allowance = hexToNum(d.result);
-            console.log("allowance:", allowance);
-            if (allowance < 1000000) {
-                $('#bg_popup').show();
-                setTimeout( $('#bg_popup').hide(), 40000);
-
-
-                $.ajax({
-                    type: "POST",
-                    url: urlInfura,
-                    dataType: 'json',
-                    async: false,
-                    data: JSON.stringify({
-                        "id": 0,
-                        "jsonrpc": '2.0',
-                        "method": "eth_getTransactionCount",
-                        "params": [openkey, "latest"]
-                    }),
-                    success: function (d) {
-                        console.log("get nonce action " + d.result);
-                        var options = {};
-                        options.nonce = d.result;
-                        options.to = erc20address;
-                        options.gasPrice = "0x737be7600"; //web3.toHex('31000000000');
-                        options.gasLimit = "0x927c0"; //web3.toHex('600000');
-                        ks.keyFromPassword("1234", function (err, pwDerivedKey) {
-                            var args = [addressContract, approveValue];
-                            var registerTx = lightwallet.txutils.functionTx(erc20abi, 'approve', args, options)
-                            var signedTx = lightwallet.signing.signTx(ks, pwDerivedKey, registerTx, sendingAddr)
-                            console.log("lightWallet sign:", signedTx)
-                            $.ajax({
-                                type: "POST",
-                                url: urlInfura,
-                                dataType: 'json',
-                                async: false,
-                                data: JSON.stringify({
-                                    "id": 0,
-                                    "jsonrpc": '2.0',
-                                    "method": "eth_sendRawTransaction",
-                                    "params": ["0x" + signedTx]
-                                }),
-                                success: function (d) {
-                                    console.log("Транзакция отправлена в сеть:", d.result);
-                                }
-                            })
-                        })
-                    }
-                })
-            }
+            _allowance = hexToNum(d.result);
+            console.log("allowance:", _allowance);
         }
-    });
-};
+    })
+    return _allowance
+}
+
+function approve(addressContract, approveValue) {
+	if(openkey == undefined){
+		return false;
+	}
+	
+    console.log("approve")
+	$('#bg_popup').show();
+	$.ajax({
+		type: "POST",
+		url: urlInfura,
+		dataType: 'json',
+		async: false,
+		data: JSON.stringify({
+			"id": 0,
+			"jsonrpc": '2.0',
+			"method": "eth_getTransactionCount",
+			"params": [openkey, "latest"]
+		}),
+		success: function (d) {
+			console.log("get nonce action " + d.result);
+			var options = {};
+			options.nonce = d.result;
+			options.to = erc20address;
+			options.gasPrice = "0x737be7600"; //web3.toHex('31000000000');
+			options.gasLimit = "0x927c0"; //web3.toHex('600000');
+			ks.keyFromPassword("1234", function (err, pwDerivedKey) {
+				console.log(err);
+				var args = [addressContract, approveValue];
+				var registerTx = lightwallet.txutils.functionTx(erc20abi, 'approve', args, options)
+				var signedTx = lightwallet.signing.signTx(ks, pwDerivedKey, registerTx, sendingAddr)
+				console.log("lightWallet sign:", signedTx)
+				$.ajax({
+					type: "POST",
+					url: urlInfura,
+					dataType: 'json',
+					async: false,
+					data: JSON.stringify({
+						"id": 0,
+						"jsonrpc": '2.0',
+						"method": "eth_sendRawTransaction",
+						"params": ["0x" + signedTx]
+					}),
+					success: function (d) {
+						console.log("The transaction was signed:", d.result);
+						if (d.result == undefined) {
+							approve(100000000000);
+						}
+					}
+				})
+			})
+		}
+	})
+}
 
 function callERC20(callname, adr) {
+	if(openkey == undefined){
+		return false;
+	}
+	
     var result;
     var callData;
     switch (callname) {
