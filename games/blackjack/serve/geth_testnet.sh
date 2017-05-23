494adf1f80a01ab986f1cbe0793c8916cdca7e23
davy42 <davydov_alexander@bk.ru>
2017-05-23 17:14:28 +0300

    Dice clear

--- games/blackjack/serve/geth_testnet.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-account=`geth --keystore keystore --testnet account list | head -n 1 | awk '{print substr($3,2,40)}'`
-geth --keystore keystore --testnet --rpc --rpccorsdomain="http://localhost:8080" --unlock $account --password serve/.pass 
