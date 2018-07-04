2c39aa21c9097d1cbe57b6d2c75d97d9fed66bb0
Ilya fanyShu <ilyashvy@gmail.com>
2018-07-04 10:01:54 +0300

    add initGame.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,9 @@ module.exports = {
   env: {
     browser: true,
   },
+  globals: {
+    DCLib: true
+  },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
